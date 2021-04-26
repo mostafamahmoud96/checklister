dbfceffd9dc8f771ccb4c604ef0dbd8bb0913abc
mostafamahmoud96 <moustafa.elagha96.com>
2021-04-26 20:25:39 +0200

    add admin middleware permissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,52 +12,12 @@
 
  <title>{{ config('app.name', 'Laravel') }}</title> </head>
  <body class="c-app">
- <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
-<div class="c-sidebar-brand d-lg-down-none">
-<svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-<use xlink:href="assets/brand/coreui.svg#full"></use>
-</svg>
-<svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
-<use xlink:href="assets/brand/coreui.svg#signet"></use>
-</svg>
-</div>
-<ul class="c-sidebar-nav">
-<li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="index.html">
-<svg class="c-sidebar-nav-icon">
-<use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
-</svg> Dashboard<span class="badge badge-info">NEW</span></a></li>
-
-<li class="c-sidebar-nav-title">Components</li>
-<li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-<svg class="c-sidebar-nav-icon">
-<use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-</svg> Base</a>
-<ul class="c-sidebar-nav-dropdown-items">
-<li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/breadcrumb.html"><span class="c-sidebar-nav-icon"></span> Breadcrumb</a></li>
-</ul>
-</li>
-<ul class="c-sidebar-nav">
-<li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                         document.getElementById('logout-form').submit();">
-                <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
-                </svg> {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </li>
-
-</ul>
-<button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
-</div>
+@include('partials.sidebar')
 <div class="c-wrapper c-fixed-components">
 <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
 <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show">
 <svg class="c-icon c-icon-lg">
-<use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
+<use xlink:href="vendors/@coresui/icons/svg/free.svg#cil-menu"></use>
 </svg>
 </button>
 <ul class="c-header-nav ml-auto mr-4">
