5d37388a60dd8b24006d39b9987d4ee0c891063d
mostafamahmoud96 <moustafa.elagha96.com>
2021-04-30 05:40:13 +0200

    crud operations for checklists

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,28 +1,60 @@
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
-<div class="c-sidebar-brand d-lg-down-none">
-<svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-<use xlink:href="assets/brand/coreui.svg#full"></use>
-</svg>
-<svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
-<use xlink:href="assets/brand/coreui.svg#signet"></use>
-</svg>
-</div>
-<ul class="c-sidebar-nav">
-<li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{route('home')}}">
-<svg class="c-sidebar-nav-icon">
-<use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
-</svg> Dashboard</a></li>
-@if(auth()->user()->is_admin)
-<li class="c-sidebar-nav-title">Admin</li>
-<li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link " href="{{route('admin.pages.index')}}">
-<svg class="c-sidebar-nav-icon">
-<use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-</svg> Pages
-</a>
-</li>
-@endif
-<ul class="c-sidebar-nav">
-<li class="c-sidebar-nav-item">
+    <ul class="c-sidebar-nav">
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('home') }}">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
+                </svg> {{ __('Dashboard') }}</a></li>
+
+        @if (auth()->user()->is_admin)
+            <li class="c-sidebar-nav-title">{{ __('Admin') }}</li>
+            <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
+                <a class="c-sidebar-nav-link"
+                   href="{{ route('admin.pages.index') }}">
+                    <svg class="c-sidebar-nav-icon">
+                        <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+                    </svg> {{ __('Pages') }}
+                </a>
+            </li>
+
+            <li class="c-sidebar-nav-title">{{ __('Manage Checklists') }}</li>
+            @foreach (\App\Models\ChecklistGroup::with('checklists')->get() as $group)
+                <li class="c-sidebar-nav-item c-sidebar-nav-dropdown c-show">
+                    <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle"
+                       href="{{ route('admin.checklist_groups.edit', $group->id) }}">
+                        <svg class="c-sidebar-nav-icon">
+                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+                        </svg> {{ $group->name }}</a>
+                    <ul class="c-sidebar-nav-dropdown-items">
+                        @foreach ($group->checklists as $checklist)
+                        <li class="c-sidebar-nav-item">
+                            <a class="c-sidebar-nav-link"
+                               href="{{ route('admin.checklist_groups.checklists.edit', [$group, $checklist]) }}">
+                                <span class="c-sidebar-nav-icon"></span>
+                                {{ $checklist->name }}</a>
+                        </li>
+                        @endforeach
+                        <li class="c-sidebar-nav-item">
+                            <a class="c-sidebar-nav-link"
+                               href="{{ route('admin.checklist_groups.checklists.create', $group) }}">{{ __('New checklist') }}</a>
+                        </li>
+                    </ul>
+                </li>
+            @endforeach
+            <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
+                <a class="c-sidebar-nav-link" href="{{ route('admin.checklist_groups.create') }}">{{ __('New checklist group') }}</a>
+            </li>
+        @endif
+
+        <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+                </svg> Base</a>
+            <ul class="c-sidebar-nav-dropdown-items">
+                <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/breadcrumb.html"><span class="c-sidebar-nav-icon"></span> Breadcrumb</a></li>
+            </ul>
+        </li>
+
+        <li class="c-sidebar-nav-item">
             <a class="c-sidebar-nav-link" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
@@ -34,7 +66,6 @@
                 @csrf
             </form>
         </li>
-
-</ul>
-<button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
+    </ul>
+    <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
 </div>
